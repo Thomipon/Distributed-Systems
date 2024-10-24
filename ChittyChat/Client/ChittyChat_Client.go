9c9ac890c67d1e1ed9671e89ba3716df5afdab1f
Thomipon <38535540+Thomipon@users.noreply.github.com>
2024-10-24 11:54:27 +0200

    Broadcasting works

--- ChittyChat/Client/ChittyChat_Client.go
+++ ChittyChat/Client/ChittyChat_Client.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"log"
-	"time"
+	"os"
+	"strings"
 
 	pb "main/proto"
 
@@ -12,19 +14,65 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-// TODO: remove this
-const (
-	defaultName = "world"
-)
+type message_stream = pb.ChittyChatService_EnterChatClient
 
 var (
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
-	name = flag.String("name", defaultName, "Name to greet")
 )
+
+func read(reader *bufio.Reader) string {
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\r\n", "", -1)
+	return strings.Replace(text, "\n", "", -1)
+}
+
+func send(stream message_stream) {
+	reader := bufio.NewReader(os.Stdin)
+
+	name_entered := false
+
+	for {
+		if !name_entered {
+			log.Println("Please enter your desired name:")
+		}
+		text := read(reader)
+		if len(text) == 0 {
+			continue
+		}
+		if text[0] == '\\' {
+			// Command
+			if text == "\\exit" {
+				log.Println("Exiting...")
+				return
+			}
+			log.Printf("Unknown command: %s", text)
+			continue
+		}
+		err := stream.Send(&pb.MessageRequest{Message: text})
+		if err != nil {
+			log.Fatalf("Error occurred while sending: %v", err)
+			continue
+		}
+		name_entered = true // First message is the name; we only ever get here when entering the name is successful. This is a bit hacky and there are problems. Ideally we would want to be able to retry but right now we just crash
+	}
+
+}
+
+func receive(stream message_stream) {
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("Error occurred while receiving: %v", err)
+			continue // TODO: This might be the wrong behaviour here; it seems to cause errors when exiting normally
+		}
+		log.Println(msg)
+	}
+
+}
 
 func main() {
 	flag.Parse()
-	// Set up a connection to the server.
+	// Set up a connection
 	connection, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect with error: %v", err)
@@ -32,16 +80,13 @@ func main() {
 	defer connection.Close()
 	client := pb.NewChittyChatServiceClient(connection)
 
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	// Contact the server without timeout
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	stream, err := client.EnterChat(ctx)
 	if err != nil {
-		log.Fatalf("Failed to publish message with error: %v", err)
+		log.Fatalf("Failed to enter chat with error: %v", err)
 	}
-	stream.Send(&pb.MessageRequest{Name: *name})
-	msg, err := stream.Recv()
-	if err == nil {
-		log.Printf("Greeting: %s", msg.GetMessage())
-	}
+	go receive(stream) // TODO: This probably wants to be somewhere else. We should probably only receive messages when we are logged in with our name
+	send(stream)
 }
--- ChittyChat/Server/ChittyChat_Server.go
+++ ChittyChat/Server/ChittyChat_Server.go
@@ -5,28 +5,83 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 
 	pb "main/proto"
 
 	"google.golang.org/grpc"
 )
 
+type message_stream = pb.ChittyChatService_EnterChatServer
+
 type server struct {
 	pb.UnimplementedChittyChatServiceServer
+	clients map[string]message_stream
+	mutex   sync.Mutex // This could be split into read/write locks or by resource but this should suffice for our case
+}
+
+type message_error struct {
+	msg string
+}
+
+func (e *message_error) Error() string {
+	return e.msg
 }
 
 var (
 	port = flag.Int("port", 50051, "The server port")
 )
 
-func (s *server) EnterChat(srv pb.ChittyChatService_EnterChatServer) error {
+func broadcast_internal(s *server, message string) {
+	log.Println(message)
+	for _, stream := range s.clients { // TODO: maybe filter out sender of the message
+		stream.Send(&pb.MessageReply{Message: message})
+	}
+}
+
+func register_client(s *server, name string, stream message_stream) bool {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	_, exists := s.clients[name]
+	if exists {
+		return false
+	}
+	s.clients[name] = stream
+	broadcast_internal(s, fmt.Sprintf("Participant %s joined Chitty-Chat at Lamport time L", name)) // TODO: timestamp
+	return true
+}
+
+func deregister_client(s *server, name string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	broadcast_internal(s, fmt.Sprintf("Participant %s left Chitty-Chat at Lamport time L", name)) // TODO: timestamp
+	delete(s.clients, name)
+}
+
+func broadcast(s *server, message string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	broadcast_internal(s, message)
+}
+
+func (s *server) EnterChat(stream message_stream) error {
+	msg, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+	name := msg.GetMessage() // first message is the requested name
+	if !register_client(s, name, stream) {
+		log.Println("Someone tried to log in as %s but this name is already taken by another user!", name)
+		return &message_error{"Name is already taken"}
+	}
+	defer deregister_client(s, name)
 	for {
-		msg, err := srv.Recv()
+		msg, err := stream.Recv()
 		if err != nil {
 			break
 		}
-		log.Printf("Received: %v", msg.GetName())
-		srv.Send(&pb.MessageReply{Message: "Hello " + msg.GetName()})
+		final_message := fmt.Sprintf("%s: %s", name, msg.GetMessage())
+		broadcast(s, final_message)
 	}
 	return nil
 }
@@ -38,7 +93,9 @@ func main() {
 		log.Fatalf("Failed to listen with error: %v", err)
 	}
 	serv := grpc.NewServer()
-	pb.RegisterChittyChatServiceServer(serv, &server{})
+	s := server{}
+	s.clients = make(map[string]message_stream)
+	pb.RegisterChittyChatServiceServer(serv, &s)
 	log.Printf("server listening at %v", listen.Addr())
 	if err := serv.Serve(listen); err != nil {
 		log.Fatalf("Failed to serve with error: %v", err)
